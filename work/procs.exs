9d726d162e4882a2848d6f2e5510b8ed4402558c
Arooj <aroojahmad@users.noreply.github.com>
2022-01-13 16:28:33 -0600

    added application for dictionary

---
+++ work/procs.exs
@@ -0,0 +1,20 @@
+defmodule Procs do
+  def hello(count) do
+    receive do
+
+      {:crash, reason } ->
+        exit(reason)
+
+      { :quit } ->
+      IO.puts "Im out"
+
+      { :add, n } ->
+      hello(count + n)
+
+      msg ->
+        IO.puts("#{count}: Hello #{inspect msg}")
+        hello(count)
+
+    end
+  end
+end
