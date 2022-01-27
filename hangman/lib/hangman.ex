f53530879e58aef82674b92d1ffde30d5530a03c
Arooj <aroojahmad@users.noreply.github.com>
2022-01-27 13:53:08 -0600

    making hangman a service

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -7,7 +7,7 @@ defmodule Hangman do
 
   @spec new_game :: game
   def new_game do
-    { :ok, pid } = Server.start_link()
+    { :ok, pid } = Hangman.Runtime.Application.start_game
     pid
   end
 
@@ -15,11 +15,10 @@ defmodule Hangman do
   def make_move(game, guess) do
     GenServer.call(game, { :make_move, guess })
   end
-  
+
   @spec tally(game) :: Type.tally()
   def tally(game) do
     GenServer.call(game, { :tally })
   end
 
 end
-
